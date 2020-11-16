8ddbe7120640ffd63e25938da4a6f01305f876c2
truizdiaz <tomiruizdiaz@gmail.com>
2020-11-16 12:06:51 -0300

    Parte 3

--- src/components/Tarea.svelte
+++ src/components/Tarea.svelte
@@ -1,19 +1,42 @@
 <script>
+    let tareas = [];
+    let nombreTarea = '';
 
+    function agregarTarea(event){
+        if(event.key === 'Enter'){
+            tareas.push(nombreTarea);
+            console.log(tareas);
+            tareas = tareas;
+            nombreTarea = '';
+        }
+    }
 </script>
 
 <div>
    <div class="container mt-5">
        <div class="row">
            <div class="col-lg-8 offset-lg-2">
-              <input type="text" class="form-control form-control-lg" placeholder="Ingresar tarea..">
+              <input type="text" class="form-control form-control-lg"
+                    on:keydown={agregarTarea}
+                    bind:value={nombreTarea}    
+                    placeholder="Ingresar tarea..">
               <br>
             </div>
            
            <div class="col-lg-6 offset-lg-3">
+               {#if tareas.length === 0}
                <div class="card p-2">
                    <h6>No hay tareas para mostrar</h6>
                </div>
+               {/if}
+
+               <ul class="list-group">
+                {#each tareas as tarea}
+                   <li class="list-group-item">
+                        <h5>{ tarea }</h5>
+                   </li>
+                   {/each}
+               </ul>
            </div>
        </div>
    </div>
